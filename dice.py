51d275241feb864791a5a882570ed9914acada74
yippieskippie24 <yippieskippie24@gmail.com>
2021-01-01 23:41:42 -0600

    dice.py actually works now
    Finished writing the dice.py script so it functions.

--- dice.py
+++ dice.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+#written by Tyler M Johnson
+#https://github.com/yippieskippie24/playing-with-python
+
 import random
 
 
@@ -10,21 +14,29 @@ print ("12")
 print ("20")
 print ("100")
 
-die_size = input("What Die should be rolled? ")
-die_count = input("How many dice should be rolled? ")
+die_size = int(input("What Die should be rolled? "))
+die_count = int(input("How many dice should be rolled? "))
 
-min = 1
-max = die_size
-x = int
-roll_again = "yes"
-single_die = ""
-total = ""
-while roll_again == "yes" or roll_again == "y":
+
+def rollDice():
+    #Variables
+    min = 1
+    max = die_size
+    x = int
+    roll_again = "yes"
+    die_array = []
+    total = int(0)
+    
+    
     print ("Rolling the dices...")
     print ("The values are....")
     for x in range(die_count):
-        single_die = random.randint(min, max)
-        print single_die
-        
-
-    roll_again = raw_input("Roll the dices again? ")
+        single_die = int(random.randint(min, max))
+        print (single_die)
+        die_array.append(single_die)
+    for x in die_array:
+        total += x
+    print("Total of dice rolled: " )
+    print(total)
+    
+rollDice()
