b91995f816b99fdb0f14cc14126b445cf252981b
yippieskippie24 <yippieskippie24@gmail.com>
2021-01-06 23:08:27 -0600

    The dice rolled now display on a single row and the total also shows on the same row as the text.

--- dice.py
+++ dice.py
@@ -3,15 +3,10 @@
 # https://github.com/yippieskippie24/playing-with-python
 
 import random
+import time
 
 # ask what size die is needed
-print("4")
-print("6")
-print("8")
-print("10")
-print("12")
-print("20")
-print("100")
+print("Typical Dice to roll: 4, 6, 8, 12, 20, 100")
 
 die_size = int(input("What Die should be rolled? "))
 die_count = int(input("How many dice should be rolled? "))
@@ -22,18 +17,20 @@ def rollDice():
     dieMin = 1
     dieMax = die_size
     die_array = []
+    die_row = []
+    die_row_final = ""
     total = int(0)
+    separator = ", "
 
     print("Rolling the dices...")
+    time.sleep(1)
     print("The values are....")
-    for x in range(die_count):
+    for roll in range(die_count):
         single_die = int(random.randint(dieMin, dieMax))
-        print(single_die)
         die_array.append(single_die)
-    for x in die_array:
-        total += x
-    print("Total of dice rolled: ")
-    print(total)
-
+    print(separator.join(map(str, die_array)))
+    for roll in die_array:
+        total += roll
+    print("Total of dice rolled: %d" % total)
 
 rollDice()
