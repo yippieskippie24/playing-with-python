f3c663ef8e95979d67c33526e90ff934fd09f16f
yippieskippie24 <yippieskippie24@gmail.com>
2021-01-06 18:07:44 -0600

    Cleaned up the code and fixed the variables

--- dice.py
+++ dice.py
@@ -1,42 +1,39 @@
 #!/usr/bin/env python3
-#written by Tyler M Johnson
-#https://github.com/yippieskippie24/playing-with-python
+# written by Tyler M Johnson
+# https://github.com/yippieskippie24/playing-with-python
 
 import random
 
-
-#ask what size die is needed
-print ("4")
-print ("6")
-print ("8")
-print ("10")
-print ("12")
-print ("20")
-print ("100")
+# ask what size die is needed
+print("4")
+print("6")
+print("8")
+print("10")
+print("12")
+print("20")
+print("100")
 
 die_size = int(input("What Die should be rolled? "))
 die_count = int(input("How many dice should be rolled? "))
 
 
 def rollDice():
-    #Variables
-    min = 1
-    max = die_size
-    x = int
-    roll_again = "yes"
+    # Variables
+    dieMin = 1
+    dieMax = die_size
     die_array = []
     total = int(0)
-    
 
-    print ("Rolling the dices...")
-    print ("The values are....")
+    print("Rolling the dices...")
+    print("The values are....")
     for x in range(die_count):
-        single_die = int(random.randint(min, max))
-        print (single_die)
+        single_die = int(random.randint(dieMin, dieMax))
+        print(single_die)
         die_array.append(single_die)
     for x in die_array:
         total += x
-    print("Total of dice rolled: " )
+    print("Total of dice rolled: ")
     print(total)
-    
+
+
 rollDice()
