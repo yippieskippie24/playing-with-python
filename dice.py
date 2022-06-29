62e86930a694b815442cd5e8e4d70ed87ca10f18
Tyler M Johnson <github@myinternet.tools>
2022-06-28 19:32:20 -0500

    Moved variables to inline variables.

--- dice.py
+++ dice.py
@@ -8,24 +8,21 @@ import time
 # ask what size die is needed
 print("Typical Dice to roll: 4, 6, 8, 12, 20, 100")
 
-die_size: int = int(input("What Die should be rolled? "))
-die_count: int = int(input("How many dice should be rolled? "))
-
-
 def roll_dice():
     # Variables
     die_min = 1
-    die_max = die_size
+    die_max = int(input("What Die should be rolled? "))
     die_array = []
     total = int(0)
     separator = ", "
 
+
+    for i in range(int(input("How many dice should be rolled? "))):
+        single_die = int(random.randint(die_min, die_max))
+        die_array.append(single_die)
     print("Rolling the dices...")
     time.sleep(1)
     print("The values are....")
-    for i in range(die_count):
-        single_die = int(random.randint(die_min, die_max))
-        die_array.append(single_die)
     print(separator.join(map(str, die_array)))
     for i in die_array:
         total += i
